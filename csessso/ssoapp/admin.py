acc5217f6d775a5de04f4f48540b6aad84ef580a
Finlay Maroney <finlay@finlaym.xyz>
2024-08-13 16:52:35 -0400

    made models verbose in admin

--- csessso/ssoapp/admin.py
+++ csessso/ssoapp/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from models import models
 
 class RoleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "email")
+    search_fields = ("name", "email")
 
 admin.site.register(models.Role, RoleAdmin)
 
 class RoleUserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("role", "user_email")
+    search_fields = ("role", "user_email")
 
 admin.site.register(models.RoleUser, RoleUserAdmin)
 
