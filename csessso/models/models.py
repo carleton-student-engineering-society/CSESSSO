b5689738c9ce9e8d1c6b88eafc6adefcd48559ae
Finlay Maroney <finlay@finlaym.xyz>
2024-08-15 21:29:44 -0400

    Added tostring method for models

--- csessso/models/models.py
+++ csessso/models/models.py
@@ -4,6 +4,12 @@ class RoleUser(models.Model):
     role = models.ForeignKey("Role", on_delete=models.CASCADE)
     user_email = models.CharField("Email", max_length=200)
 
+    def __str__(self) -> str:
+        return f"{self.role} - {self.user_email}"
+
 class Role(models.Model):
     name = models.CharField("Role", max_length=100)
     email = models.CharField("Email", max_length=200)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
